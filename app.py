d68b965896ddecbb8146ddcf00cea8c253a1d36f
Ngoc Hai Luong <130680499+NCKHtop1@users.noreply.github.com>
2024-06-07 21:40:22 +0700

    app.py

--- app.py
+++ app.py
@@ -52,7 +52,9 @@ def load_data(file_path):
     if not os.path.exists(file_path):
         st.error(f"File not found: {file_path}")
         return pd.DataFrame()
-    return pd.read_csv(file_path, parse_dates=['Datetime'], dayfirst=True).set_index('Datetime')
+    df = pd.read_csv(file_path, parse_dates=['Datetime'], dayfirst=True)
+    df.set_index('Datetime', inplace=True)
+    return df
 
 def load_portfolio_symbols(portfolio_name):
     file_path = PORTFOLIO_FILES.get(portfolio_name, '')
@@ -61,19 +63,7 @@ def load_portfolio_symbols(portfolio_name):
         return []
     return pd.read_csv(file_path)['symbol'].tolist()
 
-def ensure_datetime_compatibility(start_date, end_date, df):
-    if not isinstance(start_date, pd.Timestamp):
-        start_date = pd.Timestamp(start_date)
-    if not isinstance(end_date, pd.Timestamp):
-        end_date = pd.Timestamp(end_date)
-
-    if start_date < df.index.min():
-        start_date = df.index.min()
-    if end_date > df.index.max():
-        end_date = df.index.max()
-
-    return df.loc[start_date:end_date]
-
+# Load and filter detailed data
 def load_detailed_data(selected_stocks):
     data = pd.DataFrame()
     for sector, file_path in SECTOR_FILES.items():
@@ -85,25 +75,26 @@ def load_detailed_data(selected_stocks):
 
 class PortfolioOptimizer:
     def MSR_portfolio(self, data: np.ndarray) -> np.ndarray:
-        X = np.diff(np.log(data), axis=0)
-        mu = np.mean(X, axis=0)
-        Sigma = np.cov(X, rowvar=False)
+        X = np.diff(np.log(data), axis=0)  # Calculate log returns from historical price data
+        mu = np.mean(X, axis=0)  # Calculate the mean returns of the assets
+        Sigma = np.cov(X, rowvar=False)  # Calculate the covariance matrix of the returns
 
-        w = self.MSRP_solver(mu, Sigma)
-        return w
+        w = self.MSRP_solver(mu, Sigma)  # Use the MSRP solver to get the optimal weights
+        return w  # Return the optimal weights
 
     def MSRP_solver(self, mu: np.ndarray, Sigma: np.ndarray) -> np.ndarray:
-        N = Sigma.shape[0]
-        if np.all(mu <= 1e-8):
-            return np.zeros(N)
+        N = Sigma.shape[0]  # Number of assets (stocks)
+        if np.all(mu <= 1e-8):  # Check if mean returns are close to zero
+            return np.zeros(N)  # Return zero weights if no returns
 
-        Dmat = 2 * Sigma
-        Amat = np.vstack((mu, np.ones(N)))
-        bvec = np.array([1, 1])
-        dvec = np.zeros(N)
+        Dmat = 2 * Sigma  # Quadratic term for the optimizer
+        Amat = np.vstack((mu, np.ones(N)))  # Combine mean returns and sum constraint for constraints
+        bvec = np.array([1, 1])  # Right-hand side of constraints (1 for mean returns and sum)
+        dvec = np.zeros(N)  # Linear term (zero for this problem)
 
+        # Call the QP solver
         w = self.solve_QP(Dmat, dvec, Amat, bvec, meq=2)
-        return w / np.sum(abs(w))
+        return w / np.sum(abs(w))  # Normalize weights to sum to 1
 
     def solve_QP(self, Dmat: np.ndarray, dvec: np.ndarray, Amat: np.ndarray, bvec: np.ndarray, meq: int = 0) -> np.ndarray:
         def portfolio_obj(x):
@@ -132,6 +123,71 @@ class PortfolioOptimizer:
 
         return res.x
 
+    def GMV_portfolio(self, data: np.ndarray, shrinkage: bool = False, shrinkage_type='ledoit', shortselling: bool = True, leverage: int = None) -> np.ndarray:
+        X = np.diff(np.log(data), axis=0)
+        X = X[~np.isnan(X).any(axis=1)]  # Remove rows with NaN values
+
+        if shrinkage:
+            if shrinkage_type == 'ledoit':
+                Sigma = self.ledoit_wolf_shrinkage(X)
+            elif shrinkage_type == 'ledoit_cc':
+                Sigma = self.ledoitwolf_cc(X)
+            elif shrinkage_type == 'oas':
+                Sigma = self.oas_shrinkage(X)
+            elif shrinkage_type == 'graphical_lasso':
+                Sigma = self.graphical_lasso_shrinkage(X)
+            elif shrinkage_type == 'mcd':
+                Sigma = self.mcd_shrinkage(X)
+            else:
+                raise ValueError('Invalid shrinkage type. Choose from: ledoit, ledoit_cc, oas, graphical_lasso, mcd')
+        else:
+            Sigma = np.cov(X, rowvar=False)
+
+        if not shortselling:
+            N = Sigma.shape[0]
+            Dmat = 2 * Sigma
+            Amat = np.vstack((np.ones(N), np.eye(N)))
+            bvec = np.array([1] + [0] * N)
+            dvec = np.zeros(N)
+            w = self.solve_QP(Dmat, dvec, Amat, bvec, meq=1)
+        else:
+            ones = np.ones(Sigma.shape[0])
+            w = np.linalg.solve(Sigma, ones)
+            w /= np.sum(w)
+
+        if leverage is not None and leverage < np.inf:
+            w = leverage * w / np.sum(np.abs(w))
+
+        return w
+
+    def ledoitwolf_cc(self, returns: np.ndarray) -> np.ndarray:
+        T, N = returns.shape
+        returns = returns - np.mean(returns, axis=0, keepdims=True)
+        df = pd.DataFrame(returns)
+        Sigma = df.cov().values
+        Cor = df.corr().values
+        diagonals = np.diag(Sigma)
+        var = diagonals.reshape(len(Sigma), 1)
+        vols = var ** 0.5
+
+        rbar = np.mean((Cor.sum(1) - 1) / (Cor.shape[1] - 1))
+        cc_cor = np.matrix([[rbar] * N for _ in range(N)])
+        np.fill_diagonal(cc_cor, 1)
+        F = np.diag((diagonals ** 0.5)) @ cc_cor @ np.diag((diagonals ** 0.5))
+
+        y = returns ** 2
+        mat1 = (y.transpose() @ y) / T - Sigma ** 2
+        pihat = mat1.sum()
+
+        mat2 = ((returns ** 3).transpose() @ returns) / T - var * Sigma
+        np.fill_diagonal(mat2, 0)
+        rhohat = np.diag(mat1).sum() + rbar * ((1 / vols) @ vols.transpose() * mat2).sum()
+        gammahat = np.linalg.norm(Sigma - F, "fro") ** 2
+        kappahat = (pihat - rhohat) / gammahat
+        delta = max(0, min(1, kappahat / T))
+
+        return delta * F + (1 - delta) * Sigma
+
 def calculate_indicators_and_crashes(df, strategies):
     if df.empty:
         st.error("No data available for the selected date range.")
@@ -150,8 +206,8 @@ def calculate_indicators_and_crashes(df, strategies):
             supertrend = df.ta.supertrend(length=7, multiplier=3, append=True)
             if 'SUPERTd_7_3.0' in supertrend.columns:
                 df['Supertrend'] = supertrend['SUPERTd_7_3.0']
-                df['Supertrend Buy'] = supertrend['SUPERTd_7_3.0'] == 1
-                df['Supertrend Sell'] = supertrend['SUPERTd_7_3.0'] == -1
+                df['Supertrend Buy'] = supertrend['SUPERTd_7_3.0'] == 1  # Buy when supertrend is positive
+                df['Supertrend Sell'] = supertrend['SUPERTd_7_3.0'] == -1  # Sell when supertrend is negative
 
         if "Stochastic" in strategies:
             stochastic = df.ta.stoch(append=True)
@@ -163,20 +219,24 @@ def calculate_indicators_and_crashes(df, strategies):
 
         if "RSI" in strategies:
             df['RSI'] = ta.rsi(df['close'], length=14)
-            df['RSI Buy'] = df['RSI'] < 30
-            df['RSI Sell'] = df['RSI'] > 70
+            df['RSI Buy'] = df['RSI'] < 30  # RSI below 30 often considered as oversold
+            df['RSI Sell'] = df['RSI'] > 70  # RSI above 70 often considered as overbought
 
         peaks, _ = find_peaks(df['close'])
         df['Peaks'] = df.index.isin(df.index[peaks])
 
+        # Forward-fill peak prices to compute drawdowns
         peak_prices = df['close'].where(df['Peaks']).ffill()
         drawdowns = (peak_prices - df['close']) / peak_prices
 
+        # Mark significant drawdowns as crashes
         crash_threshold = 0.175
         df['Crash'] = drawdowns >= crash_threshold
 
+        # Filter crashes to keep only one per week (on Fridays)
         df['Crash'] = df['Crash'] & (df.index.weekday == 4)
 
+        # Adjust buy and sell signals based on crashes
         df['Adjusted Sell'] = ((df.get('MACD Sell', False) | df.get('Supertrend Sell', False) | df.get('Stochastic Sell', False) | df.get('RSI Sell', False)) &
                                 (~df['Crash'].shift(1).fillna(False)))
         df['Adjusted Buy'] = ((df.get('MACD Buy', False) | df.get('Supertrend Buy', False) | df.get('Stochastic Buy', False) | df.get('RSI Buy', False)) &
@@ -187,6 +247,7 @@ def calculate_indicators_and_crashes(df, strategies):
         st.error(f"An error occurred: {e}")
     return df
 
+# Function to apply T+ holding constraint
 def apply_t_plus(df, t_plus):
     t_plus_days = int(t_plus)
 
@@ -199,6 +260,7 @@ def apply_t_plus(df, t_plus):
 
     return df
 
+# Function to run backtesting using vectorbt's from_signals
 def run_backtest(df, init_cash, fees, direction, t_plus):
     df = apply_t_plus(df, t_plus)
     entries = df['Adjusted Buy']
@@ -217,6 +279,7 @@ def run_backtest(df, init_cash, fees, direction, t_plus):
     )
     return portfolio
 
+# Calculate crash likelihood
 def calculate_crash_likelihood(df):
     crash_counts = df['Crash'].resample('W').sum()
     total_weeks = len(crash_counts)
@@ -239,17 +302,12 @@ with st.sidebar.expander("Danh mục đầu tư", expanded=True):
             if symbols:
                 selected_symbols = st.multiselect(f'Chọn mã cổ phiếu trong {portfolio_option}', symbols, default=symbols)
                 selected_stocks.extend(selected_symbols)
-        
-        if selected_stocks:
-            st.write("Chọn ngành để lấy dữ liệu:")
-            selected_sector = st.multiselect('Chọn ngành', list(SECTOR_FILES.keys()), default=list(SECTOR_FILES.keys()))
 
-    else:
-        selected_sector = st.selectbox('Chọn ngành', list(SECTOR_FILES.keys()))
-        df_full = load_data(SECTOR_FILES[selected_sector])
-        available_symbols = df_full['StockSymbol'].unique().tolist()
-        selected_stocks = st.multiselect('Chọn mã cổ phiếu trong ngành', available_symbols)
+    selected_sector = st.selectbox('Chọn ngành để lấy dữ liệu:', list(SECTOR_FILES.keys()))
+    available_symbols = load_data(SECTOR_FILES[selected_sector])['StockSymbol'].unique().tolist()
+    selected_sector_symbols = st.multiselect('Chọn mã cổ phiếu trong ngành', available_symbols)
 
+# Portfolio tab
 with st.sidebar.expander("Thông số kiểm tra", expanded=True):
     init_cash = st.number_input('Vốn đầu tư (VNĐ):', min_value=100_000_000, max_value=1_000_000_000, value=100_000_000, step=1_000_000)
     fees = st.number_input('Phí giao dịch (%):', min_value=0.0, max_value=10.0, value=0.1, step=0.01) / 100
@@ -257,28 +315,28 @@ with st.sidebar.expander("Thông số kiểm tra", expanded=True):
     direction = "longonly" if direction_vi == "Mua" else "shortonly"
     t_plus = st.selectbox("Thời gian nắm giữ tối thiểu", [0, 1, 2.5, 3], index=0)
 
+    # New trading parameters
     take_profit_percentage = st.number_input('Take Profit (%)', min_value=0.0, max_value=100.0, value=10.0, step=0.1)
     stop_loss_percentage = st.number_input('Stop Loss (%)', min_value=0.0, max_value=100.0, value=5.0, step=0.1)
     trailing_take_profit_percentage = st.number_input('Trailing Take Profit (%)', min_value=0.0, max_value=100.0, value=2.0, step=0.1)
     trailing_stop_loss_percentage = st.number_input('Trailing Stop Loss (%)', min_value=0.0, max_value=100.0, value=1.5, step=0.1)
 
+    # Sidebar: Choose the strategies to apply
     strategies = st.multiselect("Các chỉ báo", ["MACD", "Supertrend", "Stochastic", "RSI"], default=["MACD", "Supertrend", "Stochastic", "RSI"])
 
-if selected_stocks:
-    if portfolio_options:
-        sector_data = pd.DataFrame()
-        for sector in selected_sector:
-            df = load_data(SECTOR_FILES[sector])
-            if not df.empty:
-                sector_data = pd.concat([sector_data, df[df['StockSymbol'].isin(selected_stocks)]])
-        df_full = sector_data
-    else:
-        df_full = load_data(SECTOR_FILES[selected_sector])
+# Ensure that the date range is within the available data
+if selected_stocks or selected_sector_symbols:
+    df_full = pd.DataFrame()
+    if selected_stocks:
+        df_full = load_detailed_data(selected_stocks)
+    elif selected_sector_symbols:
+        df_full = load_detailed_data(selected_sector_symbols)
 
     if not df_full.empty:
         first_available_date = df_full.index.min().date()
         last_available_date = df_full.index.max().date()
 
+        # Ensure selected date range is within the available data range
         start_date = st.date_input('Ngày bắt đầu', first_available_date)
         end_date = st.date_input('Ngày kết thúc', last_available_date)
 
@@ -294,19 +352,21 @@ if selected_stocks:
             st.error("Lỗi: Ngày kết thúc phải sau ngày bắt đầu.")
         else:
             try:
-                df_filtered = df_full[df_full['StockSymbol'].isin(selected_stocks)]
-                df_filtered = ensure_datetime_compatibility(start_date, end_date, df_filtered)
+                df_filtered = df_full.loc[start_date:end_date]
 
                 if df_filtered.empty:
                     st.error("Không có dữ liệu cho khoảng thời gian đã chọn.")
                 else:
+                    # Calculate indicators and crashes
                     df_filtered = calculate_indicators_and_crashes(df_filtered, strategies)
 
+                    # Run backtest
                     portfolio = run_backtest(df_filtered, init_cash, fees, direction, t_plus)
 
                     if portfolio is None or len(portfolio.orders.records) == 0:
                         st.error("Không có giao dịch nào được thực hiện trong khoảng thời gian này.")
                     else:
+                        # Create tabs for different views on the main screen
                         tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Tóm tắt", "Chi tiết kết quả kiểm thử", "Tổng hợp lệnh mua/bán", "Đường cong giá trị", "Mức sụt giảm tối đa", "Biểu đồ", "Danh mục đầu tư"])
 
                         with tab1:
@@ -323,6 +383,7 @@ if selected_stocks:
                             for index, value in summary_stats.items():
                                 st.markdown(f'<div class="highlight">{index}: {value}</div>', unsafe_allow_html=True)
 
+                            # Add crash details
                             crash_details = df_filtered[df_filtered['Crash']][['close']]
                             crash_details.reset_index(inplace=True)
                             crash_details.rename(columns={'Datetime': 'Ngày crash', 'close': 'Giá'}, inplace=True)
@@ -331,7 +392,8 @@ if selected_stocks:
 
                         with tab2:
                             st.markdown("**Chi tiết kết quả kiểm thử:**")
-                            st.markdown("Tab này hiển thị hiệu suất tổng thể của chiến lược giao dịch đã chọn. Bạn sẽ tìm thấy các chỉ số quan trọng như tổng lợi nhuận, lợi nhuận/lỗ, và các thống kê liên quan khác.")
+                            st.markdown("Tab này hiển thị hiệu suất tổng thể của chiến lược giao dịch đã chọn. \
+                                        Bạn sẽ tìm thấy các chỉ số quan trọng như tổng lợi nhuận, lợi nhuận/lỗ, và các thống kê liên quan khác.")
                             stats_df = pd.DataFrame(portfolio.stats(), columns=['Giá trị'])
                             stats_df.index.name = 'Chỉ số'
                             metrics_vi = {
@@ -354,7 +416,8 @@ if selected_stocks:
 
                         with tab3:
                             st.markdown("**Tổng hợp lệnh mua/bán:**")
-                            st.markdown("Tab này cung cấp danh sách chi tiết của tất cả các lệnh mua/bán được thực hiện bởi chiến lược. Bạn có thể phân tích các điểm vào và ra của từng giao dịch, cùng với lợi nhuận hoặc lỗ.")
+                            st.markdown("Tab này cung cấp danh sách chi tiết của tất cả các lệnh mua/bán được thực hiện bởi chiến lược. \
+                                        Bạn có thể phân tích các điểm vào và ra của từng giao dịch, cùng với lợi nhuận hoặc lỗ.")
                             trades_df = portfolio.trades.records_readable
                             trades_df = trades_df.round(2)
                             trades_df.index.name = 'Số giao dịch'
@@ -376,7 +439,8 @@ if selected_stocks:
                             )
                             st.plotly_chart(equity_fig)
                             st.markdown("**Đường cong giá trị:**")
-                            st.markdown("Biểu đồ này hiển thị sự tăng trưởng giá trị danh mục của bạn theo thời gian, cho phép bạn thấy cách chiến lược hoạt động trong các điều kiện thị trường khác nhau.")
+                            st.markdown("Biểu đồ này hiển thị sự tăng trưởng giá trị danh mục của bạn theo thời gian, \
+                                        cho phép bạn thấy cách chiến lược hoạt động trong các điều kiện thị trường khác nhau.")
 
                         with tab5:
                             drawdown_trace = go.Scatter(
@@ -398,7 +462,8 @@ if selected_stocks:
                             )
                             st.plotly_chart(drawdown_fig)
                             st.markdown("**Mức sụt giảm tối đa:**")
-                            st.markdown("Biểu đồ này minh họa sự sụt giảm từ đỉnh đến đáy của danh mục của bạn, giúp bạn hiểu rõ hơn về tiềm năng thua lỗ của chiến lược.")
+                            st.markdown("Biểu đồ này minh họa sự sụt giảm từ đỉnh đến đáy của danh mục của bạn, \
+                                        giúp bạn hiểu rõ hơn về tiềm năng thua lỗ của chiến lược.")
 
                         with tab6:
                             fig = portfolio.plot()
@@ -411,7 +476,8 @@ if selected_stocks:
                                 name='Sụt giảm'
                             )
                             st.markdown("**Biểu đồ:**")
-                            st.markdown("Biểu đồ tổng hợp này kết hợp đường cong giá trị với các tín hiệu mua/bán và cảnh báo sụp đổ tiềm năng, cung cấp cái nhìn tổng thể về hiệu suất của chiến lược.")
+                            st.markdown("Biểu đồ tổng hợp này kết hợp đường cong giá trị với các tín hiệu mua/bán và cảnh báo sụp đổ tiềm năng, \
+                                        cung cấp cái nhìn tổng thể về hiệu suất của chiến lược.")
                             st.plotly_chart(fig, use_container_width=True)
 
                         with tab7:
